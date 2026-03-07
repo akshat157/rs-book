8fc1d76c8c63042264dd4ce06e96d032c656c84c
Akshat <ancientfuture1337@gmail.com>
2026-03-07 17:10:12 +0900

    Update employee_registry project.

--- employee_registry/src/main.rs
+++ employee_registry/src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::io;
-use std::process::exit;
 // struct Company {
 //    departments: Vec<Department>,
 //    name: String,
@@ -14,44 +13,40 @@ use std::process::exit;
 // struct Employee {
 //     name: String,
 // }
-fn exit_program() {
-    println!("Exit.");
-    exit(0);
-}
-
 fn create_department(dept: &str, reg: &mut HashMap<String, Vec<String>>) {
-    println!("Create {dept}");
-    reg.insert(dept.to_string(), Vec::new());
-    // TODO: Error handling
-    println!("{reg:?}");
+    if reg.contains_key(dept) {
+        println!("Department already exists: '{dept}'");
+    } else {
+        reg.insert(dept.to_string(), Vec::new());
+        println!("Department created: {dept}");
+    }
 }
 
 fn delete_department(dept: &str, reg: &mut HashMap<String, Vec<String>>) {
-    println!("Delete {dept}");
-    reg.remove(&dept.to_string());
-    // TODO: Error handling
-    println!("{reg:?}");
+    match reg.remove(dept) {
+        Some(_) => println!("Department deleted: {dept}"),
+        None => println!("No department found by name {dept}"),
+    }
 }
 
 fn add_employee(emp: &str, dept: &str, reg: &mut HashMap<String, Vec<String>>) {
-    println!("Add {emp} to {dept}.");
     match reg.get_mut(dept) {
         Some(dept_emps) => {
             dept_emps.push(emp.to_string());
+            dept_emps.sort();
+            println!("Employee added: {emp} to {dept}.");
         }
         None => {
             println!("No department found by name {dept}");
         }
     }
-    println!("{reg:?}");
 }
 
 fn remove_employee(emp: &str, dept: &str, reg: &mut HashMap<String, Vec<String>>) {
-    println!("Remove {emp} from {dept}.");
     match reg.get_mut(dept) {
         Some(dept_emps) => {
             let mut i: Option<usize> = None;
-            for (idx, val) in dept_emps.iter_mut().enumerate() {
+            for (idx, val) in dept_emps.iter().enumerate() {
                 if val == emp {
                     i = Some(idx);
                     break;
@@ -60,12 +55,12 @@ fn remove_employee(emp: &str, dept: &str, reg: &mut HashMap<String, Vec<String>>
             if let Some(i) = i {
                 dept_emps.remove(i);
             }
+            println!("Employee removed: {emp} from {dept}.");
         }
         None => {
             println!("No department found by name {dept}");
         }
     }
-    println!("{reg:?}");
 }
 
 fn show_department(dept: &str, reg: &HashMap<String, Vec<String>>) {
@@ -81,8 +76,13 @@ fn show_department(dept: &str, reg: &HashMap<String, Vec<String>>) {
 }
 
 fn show_all_departments(reg: &HashMap<String, Vec<String>>) {
-    for dept in reg.keys() {
-        show_department(dept, reg);
+    // TODO: Optimize this method. Currently causes unnecessary repeated lookups on the HashMap.
+    // Can be replaced with raw implementation of the display method.
+    for (dept, emps) in reg {
+        println!("Showing employees for '{dept}'");
+        for emp in emps {
+            println!("{emp}");
+        }
     }
 }
 
@@ -92,24 +92,24 @@ fn print_invalid_prompt() {
 
 fn print_instructions() {
     println!("Use the following options to perform the desired operations.");
-    println!("create <department>\t: Create a new department named <department>.");
+    println!("1. create <department>\t: Create a new department named <department>.");
     println!(
-        "delete <department>\t: Delete the department named <department>.\n\t\t\tDeletes the list of employees in that department as well!"
+        "2. delete <department>\t: Delete the department named <department>.\n\t\t\t  Deletes the list of employees in that department as well!"
     );
     println!(
-        "show <department>\t: show the list of employees in department <department>, sorted alphabetically."
+        "3. show <department>\t: show the list of employees in department <department>, sorted alphabetically."
     );
     println!(
-        "showall\t: show the all departments in the company with all employees, sorted alphabetically."
+        "4. showall\t: show the all departments in the company with all employees, sorted alphabetically."
     );
     println!(
-        "add <employee> to <department>\t: Add employee <employee> to department <department>."
+        "5. add <employee> to <department>\t: add employee <employee> to department <department>."
     );
     println!(
-        "remove <employee> from <department>\t: Add employee <employee> to department <department>."
+        "6. remove <employee> from <department>\t: remove employee <employee> from department <department>."
     );
-    println!("help\t: print the help text.");
-    println!("quit\t: quit the program.");
+    println!("7. help\t: print the help text.");
+    println!("8. quit\t: exit the program.");
 }
 
 fn main() {
@@ -130,39 +130,39 @@ fn main() {
 
         let pieces: Vec<&str> = prompt.trim().split_ascii_whitespace().collect();
 
-        if !(pieces.len() == 1 || pieces.len() == 2 || pieces.len() == 4) {
-            print_invalid_prompt();
-            continue;
-        }
-
-        if pieces.len() == 1 {
-            if pieces[0] == "help" {
-                print_instructions();
-            } else if pieces[0] == "quit" {
-                exit_program();
-            } else if pieces[0] == "showall" {
-                show_all_departments(&registry);
-            } else {
-                print_invalid_prompt();
+        match pieces.len() {
+            1 => {
+                if pieces[0] == "help" {
+                    print_instructions();
+                } else if pieces[0] == "quit" {
+                    break;
+                } else if pieces[0] == "showall" {
+                    show_all_departments(&registry);
+                } else {
+                    print_invalid_prompt();
+                }
             }
-        } else if pieces.len() == 2 {
-            if pieces[0] == "create" {
-                create_department(pieces[1], &mut registry);
-            } else if pieces[0] == "delete" {
-                delete_department(pieces[1], &mut registry);
-            } else if pieces[0] == "show" {
-                show_department(pieces[1], &registry)
-            } else {
-                print_invalid_prompt();
+            2 => {
+                if pieces[0] == "create" {
+                    create_department(pieces[1], &mut registry);
+                } else if pieces[0] == "delete" {
+                    delete_department(pieces[1], &mut registry);
+                } else if pieces[0] == "show" {
+                    show_department(pieces[1], &registry)
+                } else {
+                    print_invalid_prompt();
+                }
             }
-        } else if pieces.len() == 4 {
-            if pieces[0] == "add" && pieces[2] == "to" {
-                add_employee(pieces[1], pieces[3], &mut registry);
-            } else if pieces[0] == "remove" && pieces[2] == "from" {
-                remove_employee(pieces[1], pieces[3], &mut registry);
-            } else {
-                print_invalid_prompt();
+            4 => {
+                if pieces[0] == "add" && pieces[2] == "to" {
+                    add_employee(pieces[1], pieces[3], &mut registry);
+                } else if pieces[0] == "remove" && pieces[2] == "from" {
+                    remove_employee(pieces[1], pieces[3], &mut registry);
+                } else {
+                    print_invalid_prompt();
+                }
             }
+            _ => print_invalid_prompt(),
         }
     }
 }
