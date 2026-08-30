08c8ad9a58c42e967c2025570fad3311a7d717e6
Akshat Malviya <ancientfuture1337@gmail.com>
2026-08-30 15:37:48 +0900

    Remove unconventional tests.rs file and move the tests to guess module.

--- guessing-game/src/guess.rs
+++ guessing-game/src/guess.rs
@@ -17,3 +17,19 @@ impl Guess {
         self.value
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(101);
+    }
+
+    #[test]
+    #[should_panic(expected = "greater than or equal to 1")]
+    fn less_than_1() {
+        Guess::new(0);
+    }
+}
