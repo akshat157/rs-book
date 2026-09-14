df964441da72c8a35af9ff5aa9bd402917a530ec
Akshat Malviya <ancientfuture1337@gmail.com>
2026-09-14 17:17:09 +0900

    Add a custom MyBox<T> with Deref impl.

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,11 +1,25 @@
+use crate::List::{Cons, Nil};
+use std::{fmt::Display, ops::Deref};
+
 enum List<T> {
     Cons(T, Box<List<T>>),
     Nil,
 }
 
-use std::fmt::Display;
+struct MyBox<T>(T);
 
-use crate::List::{Cons, Nil};
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 fn main() {
     let a = 5;
@@ -20,6 +34,17 @@ fn main() {
     let aptr = Box::new(a);
 
     assert_eq!(a, *aptr);
+
+    let aa = 5;
+    let bb = MyBox::new(aa);
+
+    assert_eq!(5, aa);
+    assert_eq!(5, *bb);
+
+    // let name = "Rust";
+    let m = MyBox::new(String::from("RustString"));
+    hello(&m);
+    hello(&(*m)[..]);
 }
 
 fn print_list<T: Display>(list: List<T>) {
@@ -30,4 +55,8 @@ fn print_list<T: Display>(list: List<T>) {
             print_list(*listlist);
         }
     }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
 }
