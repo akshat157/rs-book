8d6b5659f5c2afdce67bb3f30b94b0e8e0148180
Akshat Malviya <ancientfuture1337@gmail.com>
2026-09-15 05:08:12 +0900

    Replace example with Rc<RefCell<T>> example.

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,19 +1,23 @@
+#[derive(Debug)]
 enum List<T> {
-    Cons(T, Rc<List<T>>),
+    Cons(Rc<RefCell<T>>, Rc<List<T>>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
-use std::rc::Rc;
+use std::{cell::RefCell, ops::Deref, rc::Rc};
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("Count after creating a: {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("Count after creating b: {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("Count after creating c: {}", Rc::strong_count(&a));
-    }
-    println!("Count after c goes out of scope: {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
