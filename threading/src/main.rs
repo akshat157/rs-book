62b8f88379c334d4222f99b69f7e41b0f4a24e31
Akshat Malviya <ancientfuture1337@gmail.com>
2026-09-15 23:41:12 +0900

    Add move to thread example.

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,17 +1,10 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let t1 = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread.");
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v1 = vec![1, 2, 3];
+    let t1 = thread::spawn(move || {
+        println!("Here's the vector from thread: {v1:?}");
     });
 
-    for i in 1..5 {
-        println!("hi! number {i} from the main thread.");
-        thread::sleep(Duration::from_millis(1));
-    }
     t1.join().unwrap();
 }
