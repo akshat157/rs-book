a41cfe3b12f53c014dc6c6df119eeadfe034b6e8
Akshat Malviya <ancientfuture1337@gmail.com>
2026-09-15 21:06:49 +0900

    Add the reference cycle example using RefCell<T> and Rc<T>.

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,23 +1,40 @@
+use crate::List::{Cons, Nil};
+use std::{cell::RefCell, rc::Rc};
+
 #[derive(Debug)]
 enum List<T> {
-    Cons(Rc<RefCell<T>>, Rc<List<T>>),
+    Cons(T, RefCell<Rc<List<T>>>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
-use std::{cell::RefCell, ops::Deref, rc::Rc};
+impl<T> List<T> {
+    fn tail(&self) -> Option<&RefCell<Rc<List<T>>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
 
 fn main() {
-    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
 
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
 
-    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    println!("a rc count after b is created = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
 
-    *value.borrow_mut() += 10;
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
 
-    println!("a after = {a:?}");
-    println!("b after = {b:?}");
-    println!("c after = {c:?}");
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Following line causes stack overflow if uncommented. This happens because of the cyclic link
+    // between a and b.
+    // println!("a next item = {:?}", a.tail());
 }
